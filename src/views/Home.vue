be8ffc502d0c4b6f7ac45d8f9887d769e6cc4565
au676174 <202009898@post.au.dk>
2022-04-24 18:35:06 +0200

    Tester

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,3 +6,29 @@
     </div>
 </template>
 
+<style>
+    .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    }
+
+    .SearchForMatchButton {
+
+    position: absolute;
+    width: 284px;
+    height: 64px;
+    left: 45px;
+    top: 540px;
+
+    background: #1B8E87;
+    mix-blend-mode: normal;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 30px;
+    }
+</style>
