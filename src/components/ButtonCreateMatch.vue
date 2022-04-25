2b97a12737e3d86058b395e3311bf661618c2e09
August Borup Exner <202005782@post.au.dk>
2022-04-25 13:57:49 +0200

    [Modified] changed the font of buttons

--- src/components/ButtonCreateMatch.vue
+++ src/components/ButtonCreateMatch.vue
@@ -29,7 +29,7 @@
         text-align: center;
 
         color: #FFFFFF;
-        -webkit-text-stroke-width: 1.2px;
+        -webkit-text-stroke-width: 1.1px;
         -webkit-text-stroke-color: black;
     }
     .create-match-button img {
