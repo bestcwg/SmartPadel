91849c6ec7a3dca0ed88e133bd8ae80ded41dc3e
Niklas Thue Sørensen <202006269@post.au.dk>
2022-05-04 12:53:38 +0200

    [ADDED] Time page

---
+++ src/views/Time.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Time from '../components/TimeComponent.vue'
+import ContinueTime from '../components/ContinueTimeButton.vue'
+</script>
+
+<template>
+  <div class="time">
+    <h1 class="h1">Time</h1>
+    <div class="clock">
+    <Time></Time>
+    <ul class="conTime">
+    <ContinueTime></ContinueTime>
+    </ul>
+    <h3 class="h3">Availability</h3>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.h1{
+  color: white;
+  position: relative;
+right: -3.8cm;
+bottom: -2cm;
+}
+.clock{
+  position: relative;
+bottom: -3.5cm;
+}
+
+.conTime{
+  position: relative;
+  right: -1cm;
+  bottom: -1cm;
+}
+
+.h3{
+  color: white;
+  text-underline-position: below;
+  position: relative;
+  bottom: -1cm;
+  right: -3.8cm;
+font-size: small;
+text-decoration: underline;
+}
+
+</style>
