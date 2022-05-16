632238d4a9187508a9a4dc759284fa8de942a982
Niklas Thue Sørensen <202006269@post.au.dk>
2022-05-16 14:49:49 +0200

    [Added] new icon page

--- src/components/icons/navbar/IconProfile.vue
+++ src/components/icons/navbar/IconProfile.vue
@@ -1,11 +1,12 @@
 <template>
+
 <svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
 <router-link to="/profile">
 <g style="mix-blend-mode:difference">
 <path d="M12.5 14.1875C16.3574 14.1875 19.5312 11.0625 19.5312 7.15625C19.5312 3.29883 16.3574 0.125 12.5 0.125C8.59375 0.125 5.46875 3.29883 5.46875 7.15625C5.46875 11.0625 8.59375 14.1875 12.5 14.1875ZM18.75 15.75H16.0156C14.9414 16.2871 13.7695 16.5312 12.5 16.5312C11.2305 16.5312 10.0098 16.2871 8.93555 15.75H6.25C2.7832 15.75 0 18.582 0 22V22.7812C0 24.0996 1.02539 25.125 2.34375 25.125H22.6562C23.9258 25.125 25 24.0996 25 22.7812V22C25 18.582 22.168 15.75 18.75 15.75Z" fill="url(#paint0_linear_590_2176)"/>
 </g>
 <defs>
-<linearGradient id="paint0_linear_590_2176" x1="15" y1="-2" x2="15" y2="44.5" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint0_linear_590_2176" x1="15" y1="98.5" x2="15" y2="177.5" gradientUnits="userSpaceOnUse">
 <stop stop-color="#8F8F8F"/>
 <stop offset="1" stop-color="#8F8F8F" stop-opacity="0"/>
 </linearGradient>
@@ -13,4 +14,6 @@
 </router-link>
 </svg>
 
+
 </template>
+
